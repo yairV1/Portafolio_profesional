cd76217cbc413602f6dc18f8a34dc5cb426d5956
yairV1 <yair@example.com>
2026-08-06 16:33:40 -0500

    Configure GitHub Pages base path and nojekyll

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Portafolio_prosonal/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
